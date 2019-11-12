c5bd9c77fd0d65f009475ebe770e40c3cdf8e024
Juliane Nagao <julianens@gmail.com>
2019-11-12 20:00:55 +0100

    chore: add babel-loader and resolve extensions to webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,18 @@ const PATH_DIST = path.join(__dirname, 'dist');
 
 module.exports = {
   entry: path.join(PATH_SOURCE, 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
     path: PATH_DIST,
     filename: '[name].js',
